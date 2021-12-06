b974bc714c0c5502a8781d82d9eb85b08cc4ae5a
Johnstone Marucha <78861332+JohnstoneDev@users.noreply.github.com>
2021-12-06 10:22:04 +0300

    Add Tours File

---
+++ tours.js
@@ -0,0 +1,42 @@
+export const tours  = [
+    {
+        id:1,
+        price:"$2,000",
+        location:"Lamu Beach",
+        img:"http://www.destination360.com/africa/kenya/images/s/lamu-island.jpg",
+        days:"3",
+        details:"Kenya has been the subject of many works of art and literature, and without question, the memoirs and the movie Out of Africa have inspired many to dream of Kenya vacations, but even if you haven't read the book or seen the movie, Kenya is calling. Kenya travel is an amazing thing. Few countries can even dream about matching the Kenyan experience, and should you be fortunate enough to enjoy a holiday in Kenya, you will likely be the envy of many. At least you should be. Boasting bold landscapes, excellent beaches, world-class safaris, and so much more, Kenya is happy to share its bounty with outsiders. Speaking of sharing, you'll be sharing the Kenyan countryside with some of the most impressive animals on the planet during Kenya trips, and viewing them in the wild is about as ideal as a travel pursuit could ever be."
+    },
+
+    {
+        id:2,
+        price:"$1,500",
+        location:"Kakamega Forest Reserve",
+        img:"http://www.destination360.com/africa/kenya/images/s/kenya-holidays.jpg",
+        days:"4"
+    },
+
+    {
+        id:3,
+        price:"$2,500",
+        location:"Maasai Mara",
+        img:"http://www.destination360.com/africa/kenya/images/s/maasai-mara.jpg",
+        days:"5"
+    },
+
+    {
+        id:4,
+        price:"$800",
+        location:"Nakuru",
+        img:"http://www.destination360.com/africa/kenya/images/s/nakuru-hotels.jpg",
+        days:"8"
+    },
+
+    {
+        id:5,
+        price:"$500",
+        location:"Nairobi",
+        img:"http://www.destination360.com/africa/kenya/images/s/kenya.jpg",
+        days:"4"
+    }
+]
